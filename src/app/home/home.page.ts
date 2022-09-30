503e04dd0c95aa49e48792cec9958cea7166e4f0
Javier <javmoreno766@gmail.com>
2022-09-30 13:51:15 +0200

    initial

--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { AlertController } from '@ionic/angular';
 
 @Component({
   selector: 'app-home',
@@ -6,7 +7,53 @@ import { Component } from '@angular/core';
   styleUrls: ['home.page.scss'],
 })
 export class HomePage {
+  respuesta: number;
+  inputNumber: number;
+  estado: String = "undefined";
+  minNumber: number = 0;
+  maxNumber: number = 10;
 
-  constructor() {}
+  constructor(private alertController: AlertController) {
+    this.rebuildNumber();
+  }
+
+  rebuildNumber() {
+    this.respuesta = Math.floor(Math.random() * this.maxNumber) + this.minNumber;
+    this.tryNumber();
+    console.log(this.respuesta);
+  }
+
+  tryNumber() {
+    if (this.inputNumber == this.respuesta) {
+      this.estado = "acertado";
+      this.presentAlert();
+    } else if (this.inputNumber < this.respuesta) {
+      this.estado = "menor";
+    } else {
+      this.estado = "mayor";
+    }
+  }
+
+  async presentAlert() {
+    const alert = await this.alertController.create({
+      header: '¡Has Acertado!',
+      message: 'El numero era: ' + this.respuesta,
+      buttons: [
+        {
+          text: 'Volver a jugar',
+          handler: () => {
+            window.location.reload()
+          }
+        },
+      ]
+    });
+
+    await alert.present();
+
+
+  }
+
+  
 
 }
+
